97173a1f14fb35a80a7a2afd4ddba089a9460af7
BrigitTrujillo <BrigitTrujillo@users.noreply.github.com>
2023-04-20 23:06:35 -0500

    LAB05

---
+++ lab05/lab05/lab05/EntryDemo.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lab05
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EntryDemo : ContentPage
+    {
+        public EntryDemo()
+        {
+            InitializeComponent();
+
+            StackLayout stack = new StackLayout();
+            var entry = new Entry
+            {
+                Text = "This is a read-only Entry"
+            };
+            stack.Children.Add(entry);
+            Content = stack;
+
+
+        }
+    }
+}
---
+++ lab05/lab05/lab05/ImageResourceExtension.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+
+namespace lab05
+{
+    [Preserve(AllMembers = true)]
+    [ContentProperty(nameof(Source))]
+
+    public class Image : IMarkupExtension
+    {
+        public string Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null) 
+                return null;
+
+            var imageSource = ImageSource.FromResource(Source,
+                typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+
+}
